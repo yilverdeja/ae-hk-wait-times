25a15ca55cea8556849b776975b1c4c4829191d6
Yil Verdeja <yilverdeja@users.noreply.github.com>
2024-08-06 19:36:07 +0800

    feat: add hospital information

--- components/wait-time-sheet.tsx
+++ components/wait-time-sheet.tsx
@@ -5,9 +5,19 @@ import {
   Sheet,
   SheetContent,
   SheetDescription,
+  SheetFooter,
   SheetHeader,
   SheetTitle,
 } from "@/components/ui/sheet";
+import { getHospitalInformation } from "@/data/hospitals";
+import {
+  ExternalLink,
+  Hospital,
+  Mail,
+  MapPin,
+  Phone,
+  Printer,
+} from "lucide-react";
 
 interface WaitMapping {
   [key: string]: number;
@@ -40,21 +50,78 @@ export default function WaitTimeSheet({
   selectedHospitalWaitTime,
   onOpenChange,
 }: Props) {
+  const hospitalInformation = getHospitalInformation(selectedHospital);
+
   return (
     <Sheet open={open} onOpenChange={onOpenChange}>
       <SheetContent className="w-[90%] sm:w-[540px]">
         <SheetHeader>
           <SheetTitle className="text-left">{selectedHospital}</SheetTitle>
           <SheetDescription className="text-left">
-            Estimated Wait Time: {selectedHospitalWaitTime}
+            {hospitalInformation?.cluster} Cluster
+            {/* Estimated Wait Time: {selectedHospitalWaitTime} */}
           </SheetDescription>
         </SheetHeader>
+
         <div className="my-4">
           <HospitalWaitTimesCard
             data={hospitalData[selectedHospital as HospitalNames]}
             currentWaitTime={waitMapping[selectedHospitalWaitTime!] || 0} // Fallback to 0 if no data
             currentUpdateTime={selectedHospitalUpdateTime}
           />
+        </div>
+        <div className="flex flex-col gap-2 my-4 text-sm">
+          <h4 className="text-lg font-bold">Information</h4>
+          <div className="flex flex-row gap-2 items-center">
+            <MapPin size={20} />
+            <a
+              className="underline underline-offset-2"
+              href={hospitalInformation?.googleMapsLink}
+              target="_blank"
+            >
+              {hospitalInformation?.address}
+            </a>
+          </div>
+          <div className="flex flex-row gap-2 items-center">
+            <Phone size={20} />
+            <p>{hospitalInformation?.telephone}</p>
+          </div>
+          <div className="flex flex-row gap-2 items-center">
+            <Printer size={20} />
+            <p>{hospitalInformation?.fax}</p>
+          </div>
+          <div className="flex flex-row gap-2 items-center">
+            <Mail size={20} />
+            <a
+              className="underline underline-offset-2"
+              href={`mailto:${hospitalInformation?.email}`}
+              target="_blank"
+            >
+              {hospitalInformation?.email}
+            </a>
+          </div>
+          <div className="flex flex-row gap-2 items-center">
+            <Hospital size={20} />
+            <a
+              className="underline underline-offset-2"
+              href={hospitalInformation?.link}
+              target="_blank"
+            >
+              Hospital Authority Profile
+            </a>
+          </div>
+          {hospitalInformation?.website && (
+            <div className="flex flex-row gap-2 items-center">
+              <ExternalLink size={20} />
+              <a
+                className="underline underline-offset-2"
+                href={hospitalInformation?.website}
+                target="_blank"
+              >
+                Website
+              </a>
+            </div>
+          )}
         </div>
       </SheetContent>
     </Sheet>
