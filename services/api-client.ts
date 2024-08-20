11f4d44b1d9ebb6766366245540bee0265de9ed8
Yil Verdeja <yilverdeja@users.noreply.github.com>
2024-08-20 12:45:34 +0800

    fix: update api env string

--- services/api-client.ts
+++ services/api-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_AWS_API_GATEWAY_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
