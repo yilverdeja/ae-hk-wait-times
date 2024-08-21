e93fa184de60474e7698f8b8fa804ca6740b4c4c
Yil Verdeja <yilverdeja@users.noreply.github.com>
2024-08-21 19:47:33 +0800

    setup: testing with vitest

---
+++ __tests__/page.test.tsx
@@ -0,0 +1,10 @@
+import { expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Page from "../app/page";
+
+test("Page", () => {
+  render(<Page />);
+  expect(
+    screen.getByRole("heading", { level: 1, name: "Current A&E Waiting Times" })
+  ).toBeDefined();
+});
