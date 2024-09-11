bfd251c94d8a3d073fa3d891890ed4d553d6ab44
Yil Verdeja <yilverdeja@users.noreply.github.com>
2024-09-11 12:11:41 +0800

    fix: select error on build
    Build throws Uncaught TypeError: undefined is not iterable when the trend-selector (Select) is used in a mobile viewing port

--- next-app/app/components/hospital-trend-selector.tsx
+++ next-app/app/components/hospital-trend-selector.tsx
@@ -1,50 +1,57 @@
 import { Tabs, TabsList, TabsTrigger } from "@/app/components/shadcn/tabs";
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
+	Select,
+	SelectPortal,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
 } from "@/app/components/shadcn/select";
 import { daySelectOptions } from "@/app/lib/types";
 
 interface Props {
-  day: string;
-  onChange: (newDay: string) => void;
+	day: string;
+	onChange: (newDay: string) => void;
 }
 
 export default function HospitalTrendSelector({ day, onChange }: Props) {
-  return (
-    <>
-      <div className="block sm:hidden">
-        <Select defaultValue={day} onValueChange={onChange}>
-          <SelectTrigger>
-            <SelectValue defaultValue={day} placeholder="Day" />
-          </SelectTrigger>
-          <SelectContent>
-            {daySelectOptions.map(({ value, long }) => (
-              <SelectItem key={value} value={value}>
-                {long}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
-      </div>
-      <div className="hidden sm:block">
-        <Tabs defaultValue={day} className="w-[400px]" onValueChange={onChange}>
-          <TabsList>
-            {daySelectOptions.map(({ value, short }) => (
-              <TabsTrigger
-                key={value}
-                value={value}
-                className="text-xs px-[5px] sm:text-sm sm:px-3"
-              >
-                {short}
-              </TabsTrigger>
-            ))}
-          </TabsList>
-        </Tabs>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="block sm:hidden">
+				<Select defaultValue={day} onValueChange={onChange}>
+					<SelectTrigger>
+						<SelectValue defaultValue={day} placeholder="Day" />
+					</SelectTrigger>
+					<SelectPortal>
+						<SelectContent>
+							{daySelectOptions.map(({ value, long }) => (
+								<SelectItem key={value} value={value}>
+									{long}
+								</SelectItem>
+							))}
+						</SelectContent>
+					</SelectPortal>
+				</Select>
+			</div>
+			<div className="hidden sm:block">
+				<Tabs
+					defaultValue={day}
+					className="w-[400px]"
+					onValueChange={onChange}
+				>
+					<TabsList>
+						{daySelectOptions.map(({ value, short }) => (
+							<TabsTrigger
+								key={value}
+								value={value}
+								className="text-xs px-[5px] sm:text-sm sm:px-3"
+							>
+								{short}
+							</TabsTrigger>
+						))}
+					</TabsList>
+				</Tabs>
+			</div>
+		</>
+	);
 }
