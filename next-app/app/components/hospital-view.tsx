7cc8bf62d941bca5dcc6294b92ffefce4b6a8c90
Yil Verdeja <yilverdeja@users.noreply.github.com>
2024-09-11 11:50:40 +0800

    quick-fix: disable eslint for build

--- next-app/app/components/hospital-view.tsx
+++ next-app/app/components/hospital-view.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { sendGAEvent } from "@next/third-parties/google";
 import HospitalTable from "@/app/components/hospital-table";
 import HospitalSheet from "@/app/components/hospital-sheet";
